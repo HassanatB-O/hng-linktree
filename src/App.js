a610c2e8d1a920716d81e03c00a9a14212c494ef
HassanatB-O <hassanatsubomi@gmail.com>
2022-10-26 07:57:28 +0100

    Codebase

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Linktree from './Linktree';
+
+function App() {
+  return (
+    <div className="App">
+      <Linktree/>      
+    </div>
+  );
+}
+
+export default App;
