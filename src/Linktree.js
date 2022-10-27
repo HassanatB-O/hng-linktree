4b97b78ff877ae6172d8f46ea3834b170d833ad5
Hassanat Busari <60980025+HassanatB-O@users.noreply.github.com>
2022-10-27 10:24:16 +0100

    Update Linktree.js

--- src/Linktree.js
+++ src/Linktree.js
@@ -9,8 +9,11 @@ export default function Linktree() {
         <h2 id='twitter'>Ayobusarii</h2>
         <h2 id='slack'>HassanatB-O</h2>
         <div className='links-div'>
+            <a href='https://twitter.com/Ayobusarii'>
+                <p className='link' id='btn__zuri'>Twitter Link</p>
+            </a>
             <a href='https://training.zuri.team/'>
-                <p className='link' id='btn__zuri'>Zuri Training</p>
+                <p className='link' id='btn__zuri'>Zuri Team</p>
             </a>
             <a href='http://books.zuri.team'>
                 <div className='link'  id='books'>
@@ -20,19 +23,19 @@ export default function Linktree() {
             </a>
             <a href='https://books.zuri.team/python-for-beginners?ref_id=HassanatB-O'>
                 <div className='link' id='book__python'>
-                    <p>The Zuri Book</p>
+                    <p>Python Books</p>
                     <p className='description'>With The Zuri Book you can get vast knowledge about design and coding. The content in this book has been carefully curated by learned developers just for you.</p>
                 </div>
             </a>
             <a href='https://background.zuri.team'>
                 <div className='link' id='pitch'>
-                    <p>Background Checks for Devs</p>
+                    <p>Background Checks for Coders</p>
                     <p className='description'>Need to get information on a developer? No need to look further!</p>
                 </div>
             </a>
             <a href='https://books.zuri.team/design-rules'>
                 <div className='link' id='book__design'>
-                    <p>Design Rules</p>
+                    <p>Design Books</p>
                     <p className='description'>Are you a designer? Zuri offers access to free guidelines for you to excel at building art.</p>
                 </div>
             </a>
